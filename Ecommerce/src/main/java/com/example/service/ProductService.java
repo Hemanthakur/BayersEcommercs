9280989b4c9e2ce7017b8f069fec0486bec9e2db
Hemant <2122977@cognizant.com>
2022-12-01 20:49:25 +0530

    Final Till Cart

---
+++ Ecommerce/src/main/java/com/example/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.model.Product;
+
+public interface ProductService {
+	
+	public Product addProduct(Product product);
+	
+	public List<Product> getAllProduct();
+	
+	public int deleteProduct(Long productId);
+	
+	public void updateProduct(Product product,Long productId);
+	
+	Optional<Product> findByProductId(Long productId);
+}
