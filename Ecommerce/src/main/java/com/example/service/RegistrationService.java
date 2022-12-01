a4c06f3c4995936a40253b40594b9df66c903696
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2022-12-01 20:44:38 +0530

    Delete Ecommerce directory

--- Ecommerce/src/main/java/com/example/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.model.Seller;
-import com.example.request.SellerRequest;
-
-public interface RegistrationService {
-	public Seller createUserReq(SellerRequest sellerRequest);
-	
-}
