7d6c3cb1d25bfc6be3c13055d1f094d6d8da4a95
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2022-12-04 19:20:59 +0530

    Create OrderService.java

---
+++ FinalEcommerce/src/main/java/com/example/service/OrderService.java
@@ -0,0 +1,7 @@
+import com.example.model.Buyer;
+import com.example.request.PlaceOrderRequest;
+
+public interface OrderService {
+	
+	public void placeOrder(Buyer buyer,PlaceOrderRequest orderReques);
+}
