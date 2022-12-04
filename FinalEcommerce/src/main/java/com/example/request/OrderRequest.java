569851bad138d6e892e8c819d4fd01b7274e752c
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2022-12-04 19:30:41 +0530

    Create OrderRequest.java

---
+++ FinalEcommerce/src/main/java/com/example/request/OrderRequest.java
@@ -0,0 +1,35 @@
+package com.example.request;
+
+import javax.validation.constraints.NotNull;
+
+import com.example.model.Order;
+
+public class OrderRequest {
+    private Integer id;
+    private @NotNull Integer userId;
+
+    public OrderRequest() {
+    }
+
+    public OrderRequest(Order order) {
+        this.setId(order.getId());
+        //this.setUserId(order.getUserId());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+}
