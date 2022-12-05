9ad1481ad111bd941730ee3927619af15eb8924e
Hemant <108360260+Hemanthakur@users.noreply.github.com>
2022-12-05 12:24:29 +0530

    Delete Ecommerce directory

--- Ecommerce/src/main/java/com/example/service/BuyerLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.model.Buyer;
-import com.example.request.BuyerLoginRequest;
-
-public interface BuyerLoginService {
-	public Buyer LoginUser(BuyerLoginRequest buyerLoginRequest);
-}
--- Ecommerce/src/main/java/com/example/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.model.Seller;
-import com.example.request.LoginRequest;
-
-public interface LoginService {
-
-	public Seller LoginUser(LoginRequest loginRequest);
-}
